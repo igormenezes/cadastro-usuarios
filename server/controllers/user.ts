8f332e1e5c244bb83edeae64067e2eafd8b8f12c
Igor Menezes <imigormenezes@gmail.com>
2018-04-18 15:59:42 -0300

    Implementando rota para realizacao de login e realizando alguns ajustes.

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -2,9 +2,11 @@ import { validationUserPromise } from "../helpers/promises/validationUser";
 import { validationIdUserPromise } from "../helpers/promises/validationIdUser";
 import { saveUserPromise } from "../helpers/promises/saveUser";
 import { getUserIdPromise } from "../helpers/promises/getUserId";
+import { authenticationPassport } from "../helpers/authentication/passport";
+import { checkLogin } from "../middlewares/middlewares";
 
 export = (app: any) => {
-    app.post('/save', (req: any, res: any) => {
+    app.post('/save', (req: Express.Session, res: Express.Session) => {
         validationUserPromise(req)
             .then(() =>
                 saveUserPromise(req)
@@ -17,7 +19,7 @@ export = (app: any) => {
             })
     });
 
-    app.get('/show-user/:id', (req: any, res: any) => {
+    app.get('/show-user/:id', checkLogin, (req: Express.Session, res: Express.Session) => {
         validationIdUserPromise(req)
             .then(() =>
                 getUserIdPromise(req)
@@ -28,5 +30,17 @@ export = (app: any) => {
             .catch(reject => {
                 res.status(reject.status).send({ success: false, msg: reject.msg });
             })
+    });
+
+    app.post('/login', (req: Express.Session, res: Express.Session) => {
+        let authentication = new authenticationPassport();
+
+        authentication.initiallize(req, res)
+            .then(resolve => {
+                res.status(200).send({ success: true, data: resolve });
+            })
+            .catch(reject => {
+                res.status(reject.status).send({ success: false, msg: reject.msg });
+            });
     });
 }
