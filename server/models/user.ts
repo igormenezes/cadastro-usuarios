e8c3ddd8b297075377d5a7b6b137a70f3226e057
Igor Menezes <imigormenezes@gmail.com>
2018-04-18 16:00:20 -0300

    Realizando alguns ajustes e obtendo usuario por email e senha.

--- server/models/user.ts
+++ server/models/user.ts
@@ -2,10 +2,10 @@ import { connectionAbstract } from "../db/connectionAbstract";
 import { Query } from "mysql";
 
 export class User extends connectionAbstract {
-    public save(user: any, callback: any) {
+    public save(email: String, password: String, type: Number, callback: any) {
         let sql = 'INSERT INTO users (email, password, type) VALUES (?, ?, ?)';
 
-        this.connection.query(sql, [user.email, user.password, user.type],
+        this.connection.query(sql, [email, password, type],
             (error: String, result: any) => {
                 if (error) {
                     return callback('errorSaveUser:' + error);
@@ -27,12 +27,31 @@ export class User extends connectionAbstract {
                 }
 
                 if (result.length === 0) {
-                    callback('Usuário não encontrado!', true);
+                    return callback('Usuário não encontrado!', true);
                 }
 
                 callback(null, result[0]);
                 this.connection.end();
             }
         );
+    }
+
+    public getByEmailPassword(email: String, password: String, callback: any){
+        let sql = "SELECT * FROM users WHERE email = ? AND password = ?";
+
+        this.connection.query(sql, [email, password],
+            (error: String, result: any) => {
+                if (error) {
+                    return callback('errorSelectUserByEmailPassword:' + error);
+                }
+
+                if (result.length === 0) {
+                    return callback('Usuário não encontrado!', true);
+                }
+
+                callback(null, result[0]);
+                this.connection.end();
+            }
+        );    
     }
 }
