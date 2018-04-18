bac354708f2e376d335c21afaef7bd11afbf7248
Igor Menezes <imigormenezes@gmail.com>
2018-04-18 18:44:44 -0300

    Adicionando rota para obter dados de todos os usuarios do servidor

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -2,6 +2,7 @@ import { validationUserPromise } from "../helpers/promises/validationUser";
 import { validationIdUserPromise } from "../helpers/promises/validationIdUser";
 import { saveUserPromise } from "../helpers/promises/saveUser";
 import { getUserIdPromise } from "../helpers/promises/getUserId";
+import { getUserAllPromise } from "../helpers/promises/getUserAll";
 import { authenticationPassport } from "../helpers/authentication/passport";
 import { checkLogin } from "../middlewares/middlewares";
 
@@ -19,7 +20,7 @@ export = (app: any) => {
             })
     });
 
-    app.get('/show-user/:id', checkLogin, (req: Express.Session, res: Express.Session) => {
+    app.get('/show-user/:id', (req: Express.Session, res: Express.Session) => {
         validationIdUserPromise(req)
             .then(() =>
                 getUserIdPromise(req)
@@ -30,6 +31,16 @@ export = (app: any) => {
             .catch(reject => {
                 res.status(reject.status).send({ success: false, msg: reject.msg });
             })
+    });
+
+    app.get('/show-users', (req: Express.Session, res: Express.Session) => {
+        getUserAllPromise(req)
+        .then(resolve => {
+            res.status(200).send({ success: true, data: resolve });
+        })
+        .catch(reject => {
+            res.status(reject.status).send({ success: false, msg: reject.msg });
+        })
     });
 
     app.post('/login', (req: Express.Session, res: Express.Session) => {
