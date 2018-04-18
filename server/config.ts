be1078d90d2ee84280222738ba972319c2c36943
Igor Menezes <imigormenezes@gmail.com>
2018-04-18 15:34:47 -0300

    Realizando implementação para utilização do modulo session e passport, além de setar cors com origin para request do servidor local do angular, mantendo as credenciais. Adicionando no autoload a pasta middlewares.

--- server/config.ts
+++ server/config.ts
@@ -2,6 +2,12 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as expressValidator from 'express-validator';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import * as passportLocal from 'passport-local';
+
+export const PASSPORT = passport;
+export const PASSPORTLOCAL = passportLocal.Strategy;
 
 class Config {
     readonly app: any;
@@ -10,11 +16,25 @@ class Config {
     constructor() {
         this.consign = require('consign');
         this.app = express();
+        this.app.use(cors({
+            origin: [
+                "http://localhost:4200",
+            ],
+            credentials: true
+        }));
 
-        this.app.use(cors());
         this.app.use(bodyParser.urlencoded({ extended: true }));
         this.app.use(bodyParser.json());
         this.app.use(expressValidator());
+
+        this.app.use(session({
+            secret: 'shh128sj',
+            resave: true,
+            saveUninitialized: true
+        }));
+
+        this.app.use(passport.initialize());
+        this.app.use(passport.session());
     }
 
     public autoload(): any {
@@ -26,12 +46,12 @@ class Config {
             .include('controllers')
             .then('models')
             .then('helpers')
+            .then('middlewares')
             .then('db')
             .into(this.app);
 
         return this.app;
     }
-
 }
 
 let config: Config = new Config();
